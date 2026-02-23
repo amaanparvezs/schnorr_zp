78b6b514d8360322f147de8a11022b396385dfed
Amaan Parvez <amaanparvez.cs@gmail.com>
2026-02-23 13:21:23 +0530

    basic structure of Schnorr signature

---
+++ main.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "schnorr.h"
+
+int main() {
+    srand(time(NULL));
+
+    // (Private Key, Public Key) : (x, y)
+    long x, y;
+
+    keygen(&x, &y);
+
+    printf("Private Key (x): %ld\n", x);
+    printf("Public Key (y): %ld\n", y);
+
+    // Message to sign
+    long message;
+
+    printf("Enter a message to sign (as a long integer): ");
+    scanf("%ld", &message);
+
+    // Sign the message
+    Signature sign = schnorr_sign(x, message);
+
+    printf("Signature (R, s): (%ld, %ld)\n", sign.R, sign.s);
+
+    // Verify the signature
+    int valid = schnorr_verify(y, message, sign);
+
+    if (valid) {
+        printf("Signature is valid.\n");
+    } else {
+        printf("Signature is invalid.\n");
+    }
+
+    return 0;
+
+}
